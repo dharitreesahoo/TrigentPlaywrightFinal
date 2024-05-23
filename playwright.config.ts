8833a8b12fa5223ba911b31629353f1240769e81
dharitree <dharitreesahoo1@gmail.com>
2024-05-23 09:21:08 +0100

    updated all report4

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const config: PlaywrightTestConfig = {
   //grep:/@smoke|@regression/,
   testDir: 'tests',
   timeout: 30 * 1000,
-  testMatch: ['tests/parallelTest/**.spec.js'],
+  testMatch: ['tests/homeStorageState.spec.ts'],
   retries: 0,
   workers: 3,
   //reporter: process.env.CI ? 'blob' : 'html',
